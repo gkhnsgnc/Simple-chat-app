6df159b7fd12f802545181d548810527424c90a0
Gökhan Soğancı <gkhnsgnc@gmail.com>
2018-09-27 03:02:35 +0300

    Update chat.js

--- public/chat.js
+++ public/chat.js
@@ -1,5 +1,6 @@
 $(function () {
-    var socket = io.connect('http://localhost:3000');
+    // var socket = io.connect('http://localhost:3000');
+    var socket = io.connect(window.location.hostname);
 
     var message = $("#message");
     var username = $("#username");
